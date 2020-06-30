ad384e3596edf27cd7d81715a191cb0b07dfc396
Darrious <darriousabarger@gmail.com>
2020-06-29 21:42:15 -0400

    Setting up game loop

---
+++ Tetris.py
@@ -0,0 +1,14 @@
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
