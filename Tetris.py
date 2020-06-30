94242275b60e0f5244d91556da125c8647b32ffc
Darrious <darriousabarger@gmail.com>
2020-06-29 22:25:54 -0400

    Messing around with L block

--- Tetris.py
+++ Tetris.py
@@ -1,14 +1,27 @@
 
 import pygame
 
+
+
 pygame.init()
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([400, 500])
 
-# Run until the user asks to quit
 running = True
-while running:
+blockSize = 20
 
-    # Did the user click the window close button?
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((100, 100, 100))
+
+    
+    #drawGrid()
+    pygame.draw.rect(screen, (0,0,255), pygame.Rect(200, 200, 20, 20 ))
+    pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 200, 20, 20 ))
+    pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 220, 20, 20 ))
+    pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 240, 20, 20 ))
+    pygame.display.update()
+
+
