0fe7b2fe84f59d671d6e6c334060b9d95d9465f3
Darrious <darriousabarger@gmail.com>
2020-06-30 11:41:18 -0400

    Added block gravity and left and right movement

--- Tetris.py
+++ Tetris.py
@@ -3,37 +3,57 @@ import pygame
 
 
 # Global vars
-
+block_size = 20
+shape_l1 = []
 def draw_grid(screen):
-    blockSize = 20 #Set the size of the grid block
     for x in range(400):
         for y in range(500):
-            rect = pygame.Rect(x*blockSize, y*blockSize,
-                               blockSize, blockSize)
+            rect = pygame.Rect(x*block_size, y*block_size,
+                               block_size, block_size)
             pygame.draw.rect(screen, (80, 80, 80), rect, 1)
+
+
 
 
 def game_loop():
     pygame.init()
     screen = pygame.display.set_mode([400, 500])
-
     running = True
-    blockSize = 20
+
+    orange = (240, 120, 40)
+
+   
+    lead_x = 220
+    lead_y = 0
+    clock = pygame.time.Clock()
+    #lead_x_change = 0
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill((0, 0, 0))
-
+            if event.type == pygame.KEYDOWN:
+            	if event.key == pygame.K_LEFT:
+            		#lead_x_change = -20
+            		lead_x -= 20
+            	if event.key == pygame.K_RIGHT:
+            		#lead_x_change = 20
+            		lead_x += 20
         
-        draw_grid(screen)
-        pygame.draw.rect(screen, (0,0,255), pygame.Rect(200, 200, 20, 20 ))
-        pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 200, 20, 20 ))
-        pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 220, 20, 20 ))
-        pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 240, 20, 20 ))
-        pygame.display.update()
+        #lead_x += lead_x_change
+
+        #draw_grid(screen)
+        screen.fill((0, 0, 0))
+       	pygame.draw.rect(screen, orange, [lead_x, lead_y, 20, 20 ])
+
+       	if lead_y < 480:
+            lead_y += 20
+       
+
+        pygame.display.flip()
+        clock.tick(5)
+
 
 
 def main():
