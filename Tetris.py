f527c3e538d63cae52569dcf3c617ffb0c12778a
Darrious <darriousabarger@gmail.com>
2020-06-30 10:38:09 -0400

    Added grid

--- Tetris.py
+++ Tetris.py
@@ -2,26 +2,41 @@
 import pygame
 
 
+# Global vars
 
-pygame.init()
-screen = pygame.display.set_mode([400, 500])
-
-running = True
-blockSize = 20
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill((100, 100, 100))
-
-    
-    #drawGrid()
-    pygame.draw.rect(screen, (0,0,255), pygame.Rect(200, 200, 20, 20 ))
-    pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 200, 20, 20 ))
-    pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 220, 20, 20 ))
-    pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 240, 20, 20 ))
-    pygame.display.update()
+def draw_grid(screen):
+    blockSize = 20 #Set the size of the grid block
+    for x in range(400):
+        for y in range(500):
+            rect = pygame.Rect(x*blockSize, y*blockSize,
+                               blockSize, blockSize)
+            pygame.draw.rect(screen, (80, 80, 80), rect, 1)
 
 
+def game_loop():
+    pygame.init()
+    screen = pygame.display.set_mode([400, 500])
+
+    running = True
+    blockSize = 20
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))
+
+        
+        draw_grid(screen)
+        pygame.draw.rect(screen, (0,0,255), pygame.Rect(200, 200, 20, 20 ))
+        pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 200, 20, 20 ))
+        pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 220, 20, 20 ))
+        pygame.draw.rect(screen, (0,0,255), pygame.Rect(220, 240, 20, 20 ))
+        pygame.display.update()
+
+
+def main():
+	game_loop()
+
+main()
