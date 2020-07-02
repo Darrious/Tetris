d1d0bcd36ab11f2618436aae5c70903bee187ec0
Darrious <darriousabarger@gmail.com>
2020-07-01 21:27:51 -0400

    Attempting to implement line clears

--- Tetris.py
+++ Tetris.py
@@ -4,10 +4,10 @@ import random
 
 # Global vars
 block_size = 20
-fps = 13
+fps = 10
 
-screen_x = 400
-screen_y = 500
+screen_x = 200
+screen_y = 400
 
 shape_perm = []
 perm_color = []
@@ -17,7 +17,7 @@ yellow = (250, 250, 0)
 blue = (0, 25, 250)
 
 def shape_picker():
-	return random.randint(1, 4)
+    return random.randint(1, 4)
 
 def draw_shape(x, y, current_shape):
     shape_l1 = []
@@ -56,6 +56,28 @@ def draw_shape(x, y, current_shape):
 
     return shape_l1
 
+def line_clear(shape_y, perm_y):
+    count = 0
+    y_val = 0
+    index = []
+    for i in perm_y:
+        if i[1] == screen_y - 20:
+            count+=1
+    print(count)
+    if (count == 10):
+        print("line clear")
+        for i in range(len(perm_y)):
+        	for j in range(len(perm_y[i])):
+        		if(perm_y[i][j] == screen_y - 20):
+        			index.append(perm_y[i])
+
+        
+        for i in index:
+	        perm_y.remove(i)
+    
+    return perm_y
+
+
 def collision_check(shape, perm):
     shape_y = []
     perm_y = []
@@ -67,7 +89,8 @@ def collision_check(shape, perm):
            perm_y.append((perm[i][0], perm[i][1]))
 
 
-
+    perm_y = line_clear(shape_y,perm_y)
+    
     #print("Shape:",shape_y)
     #print("Perm:",perm_y)
     y_check = False
@@ -75,7 +98,7 @@ def collision_check(shape, perm):
     # check
     for i in shape_y:
         for j in perm_y:
-        	#y cord check
+            #y cord check
             if i[0] == j[0]:
                 if((i[1] + 25) > j[1]):
                     y_check = True
@@ -83,14 +106,25 @@ def collision_check(shape, perm):
 
             #x cord check
             if i[1] == j[1]:
-                if((i[0] + 20) == j[0]):
-            	    print("right lock")
-            	    x_check = True
-                if((i[0] - 20) == j[0]):
+                if((i[0] + 20) == j[0] or (i[0] + 20) == 350):
+                    #print("right lock")
+                    #print(i[0] + 20)
                     x_check = True
-                    print("left lock")
-
-    
+                if((i[0] - 20) == j[0] or (i[0] - 20) == 250):
+                    x_check = True
+                    #print("left lock")
+                    #print(i[1])
+            
+            '''
+            if((i[0] + 20) == 380):
+                print(i[0] + 20)
+                x_check = True
+                break
+            if((i[0] - 20) == 220):
+                x_check = True
+                print(i[1])
+                break
+            '''
     return y_check, x_check
 
 
@@ -109,25 +143,24 @@ def game_loop():
     running = True
     current_shape = random.randint(1,4)
 
-    
-  
-    lead_x = 200
+   
+    lead_x = 80
     lead_y = 0
     clock = pygame.time.Clock()
 
     while running:
         shape = draw_shape(lead_x, lead_y, current_shape)
-        check = collision_check(shape, shape_perm)
+        coll_check = collision_check(shape, shape_perm)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
             if event.type == pygame.KEYDOWN:
-                if (event.key == pygame.K_LEFT) and (not check[1]):
+                if (event.key == pygame.K_LEFT) and (not coll_check[1]):
                     lead_x -= 20
 
-                if (event.key == pygame.K_RIGHT) and (not check[1]):
+                if (event.key == pygame.K_RIGHT) and (not coll_check[1]):
                     lead_x += 20
         
 
@@ -135,31 +168,35 @@ def game_loop():
         #draw_grid(screen)
         screen.fill((0, 0, 0))
            
+
+        # Drawing shape
         for i in range(len(shape)):
             pygame.draw.rect(screen, color, shape[i])
         
         
-        if(check[0]):
+        #Checking line clear
+
+        if(coll_check[0]):
             #print("end")
             for i in range(len(shape)):
                    shape_perm.append(shape[i])
                    perm_color.append(color)
 
             lead_y = 0
-            lead_x = 200
+            lead_x = 80
             current_shape = shape_picker()
 
         if lead_y < screen_y - 20:
             lead_y += 20
         else:
-            print("end")
+            #print("end")
 
             for i in range(len(shape)):
                 shape_perm.append(shape[i])
                 perm_color.append(color)
 
             lead_y = 0
-            lead_x = 200
+            lead_x = 80
             current_shape = shape_picker()
 
 
